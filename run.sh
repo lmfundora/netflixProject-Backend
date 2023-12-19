949b0aa74b88e3890bab8e754f9b8a8eda4b3a92
lmfundora <lmfundora2@gmail.com>
2023-12-19 09:07:25 -0500

    add docjer run

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 docker image prune -f
 docker build -t backgo .
 docker stop backgo
 docker rm backgo
 
-docker run -d --name backgo -p 4000:3005 --env-file="./.env.deploi" backgo
+docker run -d --name backgo -p 3005:3005 --env-file="./.env.deploi" backgo
 docker logs -f backgo
